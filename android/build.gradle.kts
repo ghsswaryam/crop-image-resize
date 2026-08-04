c4f6525902a0579504984d89a6d94eebe3abe084
ghsswaryam <principal.ghsswaryam@gmail.com>
2026-08-04 14:11:09 +0500

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
@@ -14,7 +14,9 @@ allprojects {
 rootProject.layout.buildDirectory.set(file("../build"))
 
 subprojects {
-    project.layout.buildDirectory.set(file("${rootProject.layout.buildDirectory.get().asFile}/${project.name}"))
+    project.layout.buildDirectory.set(
+        file("${rootProject.layout.buildDirectory.get().asFile}/${project.name}")
+    )
 }
 
 subprojects {
